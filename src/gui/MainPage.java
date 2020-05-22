6fc829d4b623eb815d5d82cd1177ae5521e46110
idanrdt <52283854+idanrdt@users.noreply.github.com>
2020-05-22 17:24:21 +0300

    Update MainPage.java

--- src/gui/MainPage.java
+++ src/gui/MainPage.java
@@ -11,7 +11,6 @@ public class MainPage {
 	 */
 	public static void main(String[] args) {
 		
-		//View view = new MyCLIView(System.in, System.out);
 		View view = new LoginPageView();
 		
 		Model model = new LoginPageModel();
