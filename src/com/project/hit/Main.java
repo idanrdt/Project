4941bf0579426b6319f7af57715f83f20246f477
yair <yairpi165@gmail.com>
2020-05-30 14:01:52 +0300

    Updated

--- src/com/project/hit/Main.java
+++ src/com/project/hit/Main.java
@@ -1,18 +1,5 @@
 package com.project.hit;
 
-import com.project.hit.controller.OrderSystem;
-import com.project.hit.fileManager.EnumNameNotFoundException;
-import com.project.hit.fileManager.FileManger;
-import com.project.hit.fileManager.FileNameSelect;
-import com.project.hit.model.BankAccount;
-import com.project.hit.model.Order;
-import com.project.hit.model.Supplier;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.List;
-
 public class Main {
 
     public static void main(String[] args) {
