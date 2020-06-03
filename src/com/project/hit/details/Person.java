f4900a752671add006cd8f1f118091d64b7ba801
idanrdt <52283854+idanrdt@users.noreply.github.com>
2020-06-03 19:52:59 +0300

    PR fix

--- src/com/project/hit/details/Person.java
+++ src/com/project/hit/details/Person.java
@@ -3,6 +3,8 @@ package com.project.hit.details;
 import java.io.Serializable;
 
 public abstract class Person implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
 	protected String id;
 	protected String name;
 	protected String address;
@@ -11,6 +13,54 @@ public abstract class Person implements Serializable{
 	{
 		this.id = id;
 		this.name = name;
+		this.address = address;
+	}
+
+	/**
+	 * Gets this {@link Person} id.
+	 * @return the id of the {@link Person}.
+	 */
+	public String getId() {
+		return id;
+	}
+	
+	/**
+	 * Sets this {@link Person} id.
+	 * @param id - The id of the {@link Person}.
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	/**
+	 * Gets this {@link Person} name.
+	 * @return the name of the {@link Person}.
+	 */
+	public String getName() {
+		return name;
+	}
+	
+	/**
+	 * Sets this {@link Person} name.
+	 * @param name - The name of the {@link Person}.
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	/**
+	 * Gets this {@link Person} address.
+	 * @return the address of the {@link Person}.
+	 */
+	public String getAddress() {
+		return address;
+	}
+	
+	/**
+	 * Sets this {@link Person} address.
+	 * @param address - The address of the {@link Person}.
+	 */
+	public void setAddress(String address) {
 		this.address = address;
 	}
 
