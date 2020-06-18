fd42f7786286fbc9d307a261667e688eb338fadc
idanrdt <52283854+idanrdt@users.noreply.github.com>
2020-06-18 09:26:35 +0300

    add user table

--- src/com/project/hit/view/managerView/ManagerPageView.java
+++ src/com/project/hit/view/managerView/ManagerPageView.java
@@ -1,17 +1,46 @@
 package com.project.hit.view.managerView;
 
-public class ManagerPageView implements ManagerView {
+import javax.swing.JFrame;
 
+import com.project.hit.controller.managerController.ManagerController;
+
+public class ManagerPageView implements ManagerView {
+	
+	private ManagerController controller;
+	private JFrame mainFrame;
+	private AddUserPanel addUser;
+	private EditUserPanel editPanel;
+	
+	public ManagerPageView(ManagerController controller) {
+		this.controller = controller;
+	}
+	
 	@Override
 	public void start() {
-		// TODO Auto-generated method stub
-		
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				createAndShowGUI();
+			}
+		});		
 	}
 
 	@Override
 	public void close() {
-		// TODO Auto-generated method stub
+		mainFrame.dispose();
+	}
+
+	@Override
+	public void setController(ManagerController controller) {
+		this.controller = controller;
+	}
+	
+	private void createAndShowGUI() {
+		mainFrame = new JFrame();
+		mainFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		
+		mainFrame.pack();
+		mainFrame.setLocationRelativeTo(null);
+		mainFrame.setVisible(true);	
 	}
 
 }
--- src/com/project/hit/view/managerView/ManagerView.java
+++ src/com/project/hit/view/managerView/ManagerView.java
@@ -1,7 +1,13 @@
 package com.project.hit.view.managerView;
 
+import com.project.hit.controller.managerController.ManagerController;
 import com.project.hit.view.View;
 
 public interface ManagerView extends View {
-
+	
+	/**
+	 * Sets the controller to this view.
+	 * @param controller related to this view.
+	 */
+	public void setController(ManagerController controller);
 }
